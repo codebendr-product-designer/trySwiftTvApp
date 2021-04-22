bb2a61b45f201a7e17b88ea85973863e06fdd6a5
Daniel Saidi <daniel.saidi@gmail.com>
2021-04-22 18:35:40 +0200

    Create Apple TV app

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# trySwiftMovieApp
+# trySwiftTvApp
+
 This repository contains the SwiftUI-based tvOS app for the try! Swift World tvOS workshop.
---
+++ TvApp/TvApp.swift
@@ -0,0 +1,17 @@
+//
+//  TvAppApp.swift
+//  TvApp
+//
+//  Created by Daniel Saidi on 2021-04-22.
+//
+
+import SwiftUI
+
+@main
+struct TvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
