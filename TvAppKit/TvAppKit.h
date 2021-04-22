7f8948f079fbe6fb8950391e9e466564a637fb1e
Daniel Saidi <daniel.saidi@gmail.com>
2021-04-22 21:18:48 +0200

    Create tvOS library

---
+++ TvAppKit/TvAppKit.h
@@ -0,0 +1,18 @@
+//
+//  TvAppKit.h
+//  TvAppKit
+//
+//  Created by Daniel Saidi on 2021-04-22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TvAppKit.
+FOUNDATION_EXPORT double TvAppKitVersionNumber;
+
+//! Project version string for TvAppKit.
+FOUNDATION_EXPORT const unsigned char TvAppKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TvAppKit/PublicHeader.h>
+
+
