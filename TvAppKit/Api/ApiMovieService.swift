81dbdf74d8969fc0b3f68c89bf80b1c0b74fefe8
Daniel Saidi <daniel.saidi@gmail.com>
2021-04-23 07:59:30 +0200

    Add query to year route

--- TvAppKit/Api/ApiMovieService.swift
+++ TvAppKit/Api/ApiMovieService.swift
@@ -30,7 +30,7 @@ public class ApiMovieService: ApiService, MovieService {
     }
     
     public func getMovies(year: Int, page: Int, completion: @escaping (MovieResult<MovieList>) -> Void) {
-        let route = MovieApiRoute.moviesByYear(year, page: page, env: movieEnvironment)
+        let route = MovieApiRoute.moviesByYear(year, query: "movie", page: page, env: movieEnvironment)
         let request = route.request(for: environment)
         performTask(with: request, type: ApiMovieList.self) {
             switch $0 {
--- TvAppKit/Api/MovieApiRoute.swift
+++ TvAppKit/Api/MovieApiRoute.swift
@@ -12,7 +12,7 @@ public enum MovieApiRoute: ApiRoute {
     
     case movie(id: String, env: MovieApiEnvironment)
     case moviesBySearch(query: String, page: Int, env: MovieApiEnvironment)
-    case moviesByYear(Int, page: Int, env: MovieApiEnvironment)
+    case moviesByYear(Int, query: String, page: Int, env: MovieApiEnvironment)
     
     public var path: String { "" }
     
@@ -20,7 +20,7 @@ public enum MovieApiRoute: ApiRoute {
         switch self {
         case .movie(let id, let env): return ["i": id, "type": "movie", "plot": "full", "apikey": env.apiKey]
         case .moviesBySearch(let query, let page, let env): return ["s": query, "type": "movie", "page": "\(page)", "apikey": env.apiKey]
-        case .moviesByYear(let year, let page, let env): return ["y": "\(year)", "type": "movie", "page": "\(page)", "apikey": env.apiKey]
+        case .moviesByYear(let year, let query, let page, let env): return ["y": "\(year)", "s": query, "type": "movie", "page": "\(page)", "apikey": env.apiKey]
         }
     }
 }
