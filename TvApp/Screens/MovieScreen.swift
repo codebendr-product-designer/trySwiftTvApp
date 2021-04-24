f2c710cf4dd1f3442161e3c60e3b8379ba8a8c43
Daniel Saidi <daniel.saidi@gmail.com>
2021-04-24 17:28:26 +0200

    Add more info to the movie screen

--- TvApp/Screens/MovieScreen.swift
+++ TvApp/Screens/MovieScreen.swift
@@ -14,42 +14,81 @@ struct MovieScreen: View {
     init(
         movie: Movie,
         movieService: MovieService = IoC.movieService) {
-        self.movie = movie
         self.movieService = movieService
+        _context = StateObject(wrappedValue: ScreenContext(movie: movie))
     }
     
-    private let movie: Movie
+    class ScreenContext: ObservableObject {
+        
+        init(movie: Movie) {
+            self.movie = movie
+        }
+        
+        @Published var movie: Movie
+    }
+    
+    private var movie: Movie { context.movie }
     private let movieService: MovieService
     
+    @StateObject private var context: ScreenContext
+    
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 20) {
             Spacer()
             MovieCover(movie: movie)
                 .aspectRatio(contentMode: .fit)
                 .frame(maxHeight: 500)
-            HStack {
-                Text(movie.title).font(.title)
-                Spacer()
+                .cornerRadius(20)
+                .shadow(radius: 10)
+            
+            VStack(alignment: .leading, spacing: 10) {
+                HStack {
+                    Text(movie.title).font(.title)
+                    Spacer()
+                }
+                HStack(spacing: 30) {
+                    Text(movie.year)
+                    Text(movie.runtime)
+                    Text(movie.rated)
+                        .fixedSize(horizontal: true, vertical: false)
+                        .padding(.vertical, 5)
+                        .padding(.horizontal, 10)
+                        .border(Color.white, width: 3)
+                        .cornerRadius(5)
+                }
+                .padding(.bottom, 30)
+                .font(.caption)
             }
-            HStack(spacing: 30) {
-                Text(movie.year)
-                Text(movie.runtime)
-                Text(movie.rated)
-                    .fixedSize(horizontal: true, vertical: false)
-                    .padding(.vertical, 5)
-                    .padding(.horizontal, 10)
-                    .border(Color.white, width: 3)
-                    .cornerRadius(5)
-            }
-            .padding(.bottom, 40)
-            .font(.caption)
             
             Text(movie.plot)
+            
+            HStack(spacing: 0) {
+                Text("Actors: ").bold()
+                Text(movie.actors.joined(separator: ", "))
+            }
+            
+            HStack(spacing: 0) {
+                Text("Writers: ").bold()
+                Text(movie.writers.joined(separator: ", "))
+            }
         }.background(
             MovieCover(movie: movie)
                 .edgesIgnoringSafeArea(.all)
                 .opacity(0.2)
-        )
+        ).onAppear(perform: fetchMovie)
+    }
+}
+
+private extension MovieScreen {
+    
+    func fetchMovie() {
+        context.movie = movie
+        movieService.getMovie(id: movie.id) {
+            switch $0 {
+            case .failure: break // Handle error
+            case .success(let movie): context.movie = movie
+            }
+        }
     }
 }
 
